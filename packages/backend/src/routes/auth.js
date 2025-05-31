3d5b5e1da272e6d5b4afe468bdb558d85a342ad3
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2025-05-31 21:32:53 +0200

    Bug fix: account deletion not accepting verifying password.

--- packages/backend/src/routes/auth.js
+++ packages/backend/src/routes/auth.js
@@ -177,9 +177,9 @@ router.post('/delete-account', verifyToken, async (req, res) => {
       });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-      return res.status(400).json({ 
+      return res.status(401).json({ 
         message: 'Felaktigt lösenord',
         type: 'error'
       });
